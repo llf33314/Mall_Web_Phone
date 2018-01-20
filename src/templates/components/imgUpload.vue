3d396a3eb7d2dff2ef11d710f7ed8d85b77fa215
yangqian <3001372437@qq.com>
2018-01-20 09:56:46 +0800

    上传图片   以及  首页样式

--- src/templates/components/imgUpload.vue
+++ src/templates/components/imgUpload.vue
@@ -53,19 +53,18 @@ export default {
           return;
         }
       }
-      let file_type = e.target.name;
-      this.xuanzhuan(file[0], file_type);
-    },
-    upload(file, file_type) {
       this.$store.commit("is_show_loading", true);
+      console.log(file, "file");
       //创建form对象
       let formData = new FormData();
       formData.append("busId", _this.$store.state.busId); //添加form表单中其他数据
+      //多个图片上传;
       if (file != null && file.length > 0) {
         for (let i = 0; i < file.length; i++) {
           formData.append("file", file[i]); //通过append向form对象添加数据
         }
       }
+      formData.append("file", file); //通过append向form对象添加数据
       let config = {
         headers: {
           "Content-Type": "multipart/form-data"
@@ -93,132 +92,6 @@ export default {
         }
         _this.$emit("returnUrl", returndata);
       });
-    },
-    //旋转图片
-    xuanzhuan(f, file_type) {
-      let _this = this;
-      //图片方向角
-      let Orientation = null;
-      let oReader = new FileReader();
-      //获取照片方向角属性，用户旋转控制
-      EXIF.getData(f, function() {
-        EXIF.getAllTags(this);
-        Orientation = EXIF.getTag(this, "Orientation");
-      });
-      oReader.onload = function(e) {
-        var image = new Image();
-        let fd = new FormData();
-        image.src = e.target.result;
-        image.onload = function() {
-          var expectWidth = this.naturalWidth;
-          var expectHeight = this.naturalHeight;
-
-          if (
-            this.naturalWidth > this.naturalHeight &&
-            this.naturalWidth > 800
-          ) {
-            expectWidth = 800;
-            expectHeight = expectWidth * this.naturalHeight / this.naturalWidth;
-          } else if (
-            this.naturalHeight > this.naturalWidth &&
-            this.naturalHeight > 1200
-          ) {
-            expectHeight = 1200;
-            expectWidth = expectHeight * this.naturalWidth / this.naturalHeight;
-          }
-          var canvas = document.createElement("canvas");
-          var ctx = canvas.getContext("2d");
-          canvas.width = expectWidth;
-          canvas.height = expectHeight;
-          ctx.drawImage(this, 0, 0, expectWidth, expectHeight);
-          var base64 = null;
-          if (_this.checkAndroid()) {
-            // 修复android
-            /*var encoder = new JPEGEncoder();
-               base64 = encoder.encode(ctx.getImageData(0, 0, expectWidth, expectHeight), 80);*/
-            base64 = e.target.result;
-          } else {
-            //修复ios
-            //如果方向角不为1，都需要进行旋转
-            if (Orientation != "" && Orientation != 1) {
-              switch (Orientation) {
-                case 6: //需要顺时针（向左）90度旋转
-                  _this.rotateImg(this, "left", canvas);
-                  break;
-                case 8: //需要逆时针（向右）90度旋转
-                  _this.rotateImg(this, "right", canvas);
-                  break;
-                case 3: //需要180度旋转
-                  _this.rotateImg(this, "right", canvas); //转两次
-                  _this.rotateImg(this, "right", canvas);
-                  break;
-              }
-            }
-            base64 = canvas.toDataURL("image/jpeg", 0.8);
-          }
-          _this["img_" + file_type] = base64;
-          fd.append("file", f);
-          _this.upload(fd, file_type);
-        };
-      };
-      oReader.readAsDataURL(f);
-    },
-    //对图片旋转处理
-    rotateImg: function(img, direction, canvas) {
-      //最小与最大旋转方向，图片旋转4次后回到原方向
-      var min_step = 0;
-      var max_step = 3;
-      if (img == null) return;
-      //img的高度和宽度不能在img元素隐藏后获取，否则会出错
-      var height = img.height;
-      var width = img.width;
-      var step = 2;
-      if (step == null) {
-        step = min_step;
-      }
-      if (direction == "right") {
-        step++;
-        //旋转到原位置，即超过最大值
-        step > max_step && (step = min_step);
-      } else {
-        step--;
-        step < min_step && (step = max_step);
-      }
-      //旋转角度以弧度值为参数
-      var degree = step * 90 * Math.PI / 180;
-      var ctx = canvas.getContext("2d");
-      switch (step) {
-        case 0:
-          canvas.width = width;
-          canvas.height = height;
-          ctx.drawImage(img, 0, 0);
-          break;
-        case 1:
-          canvas.width = height;
-          canvas.height = width;
-          ctx.rotate(degree);
-          ctx.drawImage(img, 0, -height);
-          break;
-        case 2:
-          canvas.width = width;
-          canvas.height = height;
-          ctx.rotate(degree);
-          ctx.drawImage(img, -width, -height);
-          break;
-        case 3:
-          canvas.width = height;
-          canvas.height = width;
-          ctx.rotate(degree);
-          ctx.drawImage(img, -width, 0);
-          break;
-      }
-    },
-    checkAndroid() {
-      let s = this.commonFn.isSystem();
-      if (s == "ios") {
-        return false;
-      }
-      return true;
     }
   }
 };
